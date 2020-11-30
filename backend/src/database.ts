2a8a83b81eafba73a546a9b736c4bcc4ca04dff4
Alquimar Almeida <alquimars@gmail.com>
2020-11-30 01:10:53 -0300

    Utilizando dotenv para ambiente de desenvolvimento

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,12 @@
+require('dotenv/config');
 import {Sequelize} from 'sequelize';
 
-const sequelize = new Sequelize('mysql://root:H3l3n@240816@localhost:3306/pitu');
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT
+const name = process.env.DB_NAME
+
+const sequelize = new Sequelize('mysql://'+ user +':'+ password +'@'+ host +':'+ port + '/' + name);
 
 export  default sequelize;
